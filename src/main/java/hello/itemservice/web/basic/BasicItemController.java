fda38fcca4aa1a440897748cf56d9ec2dfeeb5dc
TheMoon <woghd1008@gmail.com>
2026-08-29 20:48:26 +0900

    feat: PRG Psot/Redirect/Get 구현

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -80,6 +80,12 @@ public class BasicItemController {
         return "basic/item";
     }
 
+//    @PostMapping("/add")
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+        return "redireect:/basic/items/" + item.getId();
+    }
+
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
